6f9fe4120fd859f3cda83bd1a1349b1713bb435a
xgqfrms <xgqfrms@outlook.com>
2016-10-26 04:41:47 +0800

    babel (OK：传递 相对路径 path)
    # babel index.js -w -o index-compiled.js
    ## --out-file or -o
    ## --watch or -w
    
    ```sh
    # ? local path 不好使
    $ ./node_modules/.bin/babel src -d lib
    
    # cd 进入babel.exe 所在的 ./node_modules/.bin/ 目录
    $ cd ./node_modules/.bin/
    
    # 先copy 文件到该目录，再执行 (OK：传递path)
    $ babel index.js -w -o index-compiled.js
    
    # (OK：传递 相对路径 path)
    
    $ babel ../../index.js -w -o ../../index-compiled.js
    ```
    
    # 推荐用法：(package.json)
    
    ```json
    {
    "name": "React-App",
    "version": "1.0.1",
    "scripts": {
    "build": "babel src -d lib"
    },
    "devDependencies": {
    "babel-cli": "^6.0.0"
    }
    }
    
    ```

--- dev-React/codepen-react-demos/gulpfile.js
+++ dev-React/codepen-react-demos/gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var react = require('gulp-react');
+var babel = require('gulp-babel');
+const concat = require('gulp-concat');
 
 // --save-dev or --save 
 // require 相对路径，
@@ -11,6 +13,8 @@ var react = require('gulp-react');
 //npm install --save-dev gulp-react
 //npm install --save-dev gulp-sourcemaps
 
+//$ npm install --save-dev babel-plugin-transform-runtime
+//$ npm install --save-dev gulp-concat
 
 // https://egghead.io/lessons/react-react-fundamentals-development-environment-setup?course=react-fundamentals#/tab-transcript
 
@@ -20,6 +24,8 @@ var react = require('gulp-react');
 // webpack.config.js main.js app.js index.html
 
 
+/*
+// .jsx
 gulp.task('default', function () {
     return gulp.src('ticking.jsx')
         .pipe(sourcemaps.init())
@@ -27,8 +33,19 @@ gulp.task('default', function () {
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dest/'));
 });
+*/
 
-
+gulp.task('default', function () {
+    return gulp.src('ticking.jsx')
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+             presets: ['es2015']
+         }))
+        .pipe(react())
+        // .pipe(concat('all.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./'));
+});
 
 
 
---
+++ dev-React/codepen-react-demos/local path test OK/index-compiled.js
@@ -0,0 +1,27 @@
+"use strict";
+// [error tutorial](https://babeljs.io/docs/plugins/preset-es2015/)
+
+// [NPM : babel-preset-es2015](https://www.npmjs.com/package/babel-preset-es2015)
+/*
+>$ babel index.js --presets es2015 
+*/
+
+// input codes
+
+var n = 3;
+console.log([1, 2, 3].map(n = +1));
+
+var square = function square(n) {
+    return n * n;
+};
+
+/*
+// output result
+
+var n = 3;
+console.log([1, 2, 3].map(n = + 1)); 
+
+var square = function square(n) {
+    return n * n;
+};
+*/
