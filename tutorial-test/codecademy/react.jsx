ec33656523db806bec68dbda20579e6d40835ea4
xgqfrms <xgqfrms@outlook.com>
2016-11-30 21:41:50 +0800

    meta
    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
    <meta name="viewport" content="width=device-width, initial-scale=1.0,
    minimum-scale=0.5, maximum-scale=3.0">

--- tutorial-test/codecademy/react.jsx
+++ tutorial-test/codecademy/react.jsx
@@ -1,25 +1,25 @@
 
 var theGoogle = (
-  <a href="https://www.google.net">
-    <h1>
-      Click me I am Goooooogle
+    <a href="https://www.google.net">
+        <h1>
+            Click me I am Goooooogle
     </h1>
-  </a>
-)
+    </a>
+);
 
 // 还有，我们还没有提到一个规律：一个JSX表达式必须有一个确切的最外层元素。
 
 // In other words, this code will work:
 var paragraphs = (
-  <div id="i-am-the-outermost-element">
-    <p>I am a paragraph.</p>
-    <p>I, too, am a paragraph.</p>
-  </div>
+    <div id="i-am-the-outermost-element">
+        <p>I am a paragraph.</p>
+        <p>I, too, am a paragraph.</p>
+    </div>
 );
 
 // But this code will not work:
 
 var paragraphs = (
-  <p>I am a paragraph.</p> 
-  <p>I, too, am a paragraph.</p>
+    <p>I am a paragraph.</p>
+    <p>I, too, am a paragraph.</p>
 );
