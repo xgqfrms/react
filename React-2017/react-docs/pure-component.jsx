31f4615c23b19cf1ee98ebf957700594b7d82fc4
xgqfrms <xgqfrms@outlook.com>
2017-06-29 00:38:03 +0800

    PureComponent 源码分析
    https://github.com/facebook/react/issues/2517
    
    https://github.com/zhouzhongyuan/qa/issues/43
    
    https://segmentfault.com/a/1190000006741060

--- React-2017/react-docs/pure-component.jsx
+++ React-2017/react-docs/pure-component.jsx
@@ -24,3 +24,40 @@ https://60devs.com/pure-component-in-react.html
 https://hackernoon.com/react-purecomponent-considered-harmful-8155b5c1d4bc
 
 */
+
+
+/*
+
+https://github.com/brigand/react-update-if-changed
+
+
+https://github.com/brigand/react-update-if-changed#updateifchanged-component
+
+
+
+exports.shouldComponentUpdate = require('./shouldComponentUpdate');
+exports.UpdateIfChanged = require('./UpdateIfChanged');
+
+
+
+https://github.com/brigand/react-update-if-changed/blob/master/src/UpdateIfChanged.js
+
+https://github.com/brigand/react-update-if-changed/blob/master/src/shouldComponentUpdate.js
+
+
+
+https://60devs.com/pure-component-in-react.html
+
+使用React.PureComponent可以大大提高性能，因为它可以减少应用程序中的渲染操作数量。
+
+https://github.com/facebook/react/issues/2517
+
+
+https://github.com/zhouzhongyuan/qa/issues/43
+
+PureComponent 源码分析
+
+
+https://segmentfault.com/a/1190000006741060
+
+*/
