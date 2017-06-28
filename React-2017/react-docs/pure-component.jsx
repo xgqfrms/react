a2a305abc564a998bbd75f816c0ddfbb455cad68
xgqfrms <xgqfrms@outlook.com>
2017-06-29 00:20:38 +0800

    React.PureComponent & PureComponent

---
+++ React-2017/react-docs/pure-component.jsx
@@ -0,0 +1,26 @@
+/*
+
+React.PureComponent
+
+
+
+https://gist.github.com/xgqfrms-GitHub/2000c356ad8cedd00f2c74d861eda215
+
+
+https://facebook.github.io/react/docs/react-api.html
+
+https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md#rule-details
+
+
+React Top-Level API
+
+
+https://gist.github.com/xgqfrms-GitHub/b95f1824f2ea5a2e28b09dff6fd191bc#gistcomment-2135012
+
+
+
+https://60devs.com/pure-component-in-react.html
+
+https://hackernoon.com/react-purecomponent-considered-harmful-8155b5c1d4bc
+
+*/
