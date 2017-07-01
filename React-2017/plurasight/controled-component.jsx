795148e638a50e30183caa73222b52870583a913
xgqfrms <xgqfrms@outlook.com>
2017-07-01 23:21:49 +0800

    Fetch & Axios & React Docs Update
    https://gist.github.com/xgqfrms-GitHub/e59f6eca03da7c1960013cc94e04e84f#gistcomment-2137484
    
    https://react2.webgeeker.xyz/docs/component-api.html

---
+++ React-2017/plurasight/controled-component.jsx
@@ -0,0 +1,106 @@
+// controled components
+
+/*
+
+https://app.pluralsight.com/player?course=react-js-getting-started&author=samer-buna&name=react-js-getting-started-m2&clip=2&mode=live
+
+*/
+
+this.state.
+
+const changeHandler = (e) => {
+    return(
+        this.setState({
+            userName: e.target.value
+        });
+    );
+};
+
+
+
+
+value={this.state.userName}
+
+onChange={(e) => this.setState({userName: e.target.value})}
+
+onChange={(e) => this.changeHandler(e)}
+
+
+
+const user = (user="xgqfrms") => {
+    return user;
+};
+
+
+user_name = user();
+
+let url = `https://api.github.com/users/${user_name}`;
+
+// get(`https://api.github.com/users/xgqfrms`)
+
+axios.get(url)
+.then(
+    res => {
+        console.log(`response =`, res);
+        // no need response.json();
+        this.props.onSubmit(res.data);
+        console.log(`response.data = `, res.data);
+        return res.data;
+    }
+);
+
+
+fetch(`https://api.github.com/users/xgqfrms`)
+.then(
+    (response) => {
+        console.log(`response`, response);
+        let json = response.json();
+        console.log(`json`, json);
+        return json;
+    }
+)
+.then(
+    (data) => {
+        console.log(`data`, data);
+    }
+);
+
+
+axios.get(`https://api.github.com/users/xgqfrms`)
+.then(
+    res => {
+        console.log(`response`, res);
+        // no need response.json();
+        return res;
+    }
+)
+.then(
+    (data) => {
+        console.log(`data`, data);
+    }
+);
+
+
+
+
+
+
+
+const addNewCard = (card) => {
+    this.setState((prevState) => {
+        cards: prevState.cards.concat(card)
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
