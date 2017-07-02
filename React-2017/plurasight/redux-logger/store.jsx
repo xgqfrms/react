b71e3fc891d0eb4db084b3bda0af1bb8fb41b512
xgqfrms <xgqfrms@outlook.com>
2017-07-02 23:40:59 +0800

    ng-book2 & Mobx & react

--- React-2017/plurasight/redux-logger/store.jsx
+++ React-2017/plurasight/redux-logger/store.jsx
@@ -21,17 +21,65 @@ We will now import it, and pass it to createStore().
 我们现在将导入它，并将其传递给createStore（）。
 */
 
+let store = createStore(todoApp, window.STATE_FROM_SERVER);
+
+
+/*
+
+You may optionally specify the initial state as the second argument to createStore(). 
+This is useful for hydrating the state of the client to match the state of a Redux application running on the server.
+
+
+您可以选择将初始状态指定为createStore（）的第二个参数。
+这对于保持客户端的状态以匹配在服务器上运行的Redux应用程序的状态非常有用。
+
+*/
+
+
+import {
+    addTodo,
+    toggleTodo,
+    setVisibilityFilter,
+    VisibilityFilters
+} from './actions';
+
+// Log the initial state
+console.log(store.getState());
+
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+let unsubscribe = store.subscribe(() =>
+    console.log(store.getState());
+);
+
+// Dispatch some actions
+store.dispatch(addTodo('Learn about actions'));
+store.dispatch(addTodo('Learn about reducers'));
+store.dispatch(addTodo('Learn about store'));
+store.dispatch(toggleTodo(0));
+store.dispatch(toggleTodo(1));
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+
+// Stop listening to state updates
+unsubscribe();
 
 
 
 
 
 
+/*
 
+In the previous sections, we defined the actions that represent the facts about “what happened” and the reducers that update the state according to those actions.
 
+The Store is the object that brings them together. 
+The store has the following responsibilities:
 
+Holds application state;
+Allows access to state via getState();
+Allows state to be updated via dispatch(action);
+Registers listeners via subscribe(listener);
+Handles unregistering of listeners via the function returned by subscribe(listener).
 
-
-
-
+*/
 
