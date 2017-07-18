4d7bcd40185597a8adde77bc3ae4ff782a704112
xgqfrms <xgqfrms@outlook.com>
2017-07-18 23:43:41 +0800

    react-redux & redux

---
+++ Redux-2017/react-redux-all-in-one/rraio/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import {Provider} from 'react-redux';
+import ReduxApp from './redux/index.js';
+
+ReactDOM.render(
+    <ReduxApp />,
+    document.getElementById('root')
+);
+
+// PWA
+registerServiceWorker();
---
+++ Redux-2017/react-redux-all-in-one/rraio/src/redux/components/TodoList.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Todo from './Todo';
+
+const TodoList = ({ todos, onTodoClick }) => {
+    return(
+        <ul>
+            {
+                todos.map((todo) => {
+                    return(
+                        <Todo
+                            key={todo.id}
+                            {...todo}
+                            onClick={() => onTodoClick(todo.id)}
+                        />
+                    );
+                })
+            }
+        </ul>
+    );
+};
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            completed: PropTypes.bool.isRequired,
+            text: PropTypes.string.isRequired
+        }).isRequired
+    ).isRequired,
+    onTodoClick: PropTypes.func.isRequired
+};
+
+export default TodoList;
